72f89c65d446f112cb34b7b5653fe38cf01e4216
Wenyi Han <wenyi@hanwenyi.com>
2020-09-10 23:06:51 +0900

    よくわからん

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+QString getString();
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+int count=0;
+
+void MainWindow::on_pushButton_clicked()
+{
+    count++;
+    ui->label->setText(getString());
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    count=0;
+    ui->label->setText(getString());
+}
+
+QString getString ()
+{
+    return "button clicked " + QString::number(count) + " times.";
+}
